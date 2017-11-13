440fe396b743f53b30707afdb40bbcd5f08900c9
Mikkel Lehrmann <mikkellehrmann@Mikkels-MacBook-Pro.local>
2017-11-13 10:31:16 +0100

    j

---
+++ javascript.js
@@ -0,0 +1,10 @@
+$(window).on("load", startHistorie);
+
+function startHistorie() {
+    console.log("startHistorie");
+
+    $("#titel_container").removeClass("titel_start_pos");
+    $("#titel_container").addClass("titel_ned");
+}
+
+/*******************************/
